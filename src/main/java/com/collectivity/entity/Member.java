61eb1d151b4b869511ecd27da64999c1af300dc6
Miranto <hei.finoana.5@gmail.com>
2026-04-22 08:33:10 +0300

    feat: add SQL insert functionality for user data

--- src/main/java/com/collectivity/entity/Member.java
+++ src/main/java/com/collectivity/entity/Member.java
@@ -6,24 +6,25 @@ import java.time.LocalDate;
 import java.util.List;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 @ToString
 @EqualsAndHashCode
 public class Member {
-    public String           id;
-    public String           firstName;
-    public String           lastName;
-    public LocalDate        birthDate;
-    public Gender           gender;
-    public String           address;
-    public String           profession;
-    public String           phoneNumber;
-    public String           email;
+    public Long id;
+    public String firstName;
+    public String lastName;
+    public LocalDate birthDate;
+    public Gender gender;
+    public String address;
+    public String profession;
+    public String phoneNumber;
+    public String email;
     public MemberOccupation occupation;
-    public String           collectivityId;
-    public LocalDate        joinDate;
+    public Long collectivityId;
+    public boolean registrationFeePaid;
+    public boolean membershipDuesPaid;
+    public List<Member> referees;
 
-    public List<Member>     referees;
+    public Member() {}
 }
