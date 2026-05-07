75c1e7f6a70dcad213883b2f68ef3fed61b44de1
tsiory <hei.tsiory.234@gmail.com>
2026-05-07 12:28:48 +0300

    add API key security filter

--- src/main/java/edu/hei/school/agricultural/entity/MemberOccupation.java
+++ src/main/java/edu/hei/school/agricultural/entity/MemberOccupation.java
@@ -1,5 +1,5 @@
 package edu.hei.school.agricultural.entity;
 
 public enum MemberOccupation {
-    JUNIOR, SENIOR, SECRETARY, TREASURER, VICE_PRESIDENT, PRESIDENT
+    JUNIOR, SENIOR, SECRETARY, TREASURER, VICE_PRESIDENT, PRESIDENT,CONFIRMED
 }
